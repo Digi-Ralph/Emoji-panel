bb2b6f798561fa43fd90d7c30a4d064902620f7f
Digi_Ralph <Richardphone119@gmail.com>
2021-02-26 07:47:36 +0100

    initial commit

--- javascript.js
+++ javascript.js
@@ -12,17 +12,17 @@ const Emoji_panel = document.querySelector('.Emoji-panel')
 const api = './node_modules/emoji-datasource-twitter/emoji.json'
 // Fetching Data
 function searchCharacter() {
-        fetch(api)
-        .then(function(e){
-        return e.json()
+    fetch(api)
+        .then(function (e) {
+            return e.json()
         })
-        .then(function(data) {
-        let Emojicharacters = [];
-        Emojicharacters.push(...data)
-        newEmoji(Emojicharacters)
-        setUpToolTip()
-    })
-    .catch( err => console.error(err)) 
+        .then(function (data) {
+            let Emojicharacters = [];
+            Emojicharacters.push(...data)
+            newEmoji(Emojicharacters)
+            setUpToolTip()
+        })
+        .catch(err => console.error(err))
 }
 searchCharacter();
 
@@ -31,20 +31,20 @@ searchCharacter();
 let newEmoji = (Emojicharacters) => {
     let charactersEmoji = [];
     Emojicharacters.filter(e => {
-        for (let i = 0 ; i < Emojicharacters.length; i++) { 
+        for (let i = 0; i < Emojicharacters.length; i++) {
             i = {
-                name:e.name,
-                short_name:e.short_name,
-                short_names:e.short_names,
-                category:e.category,
-                image:e.image,
-                unified:e.unified,
-                sort_order:e.sort_order
+                name: e.name,
+                short_name: e.short_name,
+                short_names: e.short_names,
+                category: e.category,
+                image: e.image,
+                unified: e.unified,
+                sort_order: e.sort_order
             }
             if ((e.name && e.short_name && e.short_names && e.category && e.image && e.unified) !== null && e.category !== "People & Body") {
                 return charactersEmoji.push(i)
-                
-            }  
+
+            }
             charactersEmoji.filter(e => {
                 if (e.category === ('Skin Tones' && 'Smileys & Emotion')) {
                     return e.category = "People"
@@ -63,9 +63,9 @@ let newEmoji = (Emojicharacters) => {
         } else if (bandA > bandB) {
             comparison = -1;
         }
-            return comparison;
+        return comparison;
     }
-    
+
     charactersEmoji.sort(compare);
 
     Emoji_icons(charactersEmoji)
@@ -83,161 +83,169 @@ function content(e) {
     </div>  
     <div class="Emoji-tooltip"></div>
     </div>`;
-    
+
 }
 
 // Default Emoji icons 
 const Emoji_icons = (charactersEmoji) => {
-    charactersEmoji.filter( e => {
-            if (e.category === "People") {
-                tabs_array[1].insertAdjacentHTML("afterbegin" , content(e))
-                content(e);
-                }
-            } 
-        )
-        
-        const Animals  =  document.querySelector('#Animals')
-        const Food     =  document.querySelector('#Food')
-        const Activities =  document.querySelector('#Activities')
-        const Travel   =  document.querySelector('#Travel')
-        const Objects  =  document.querySelector('#Objects')
-        const Symbols   =  document.querySelector('#Symbols')
-        const Flags     =  document.querySelector('#Flags')
-
-    Emoji_list.addEventListener("scroll",function(event){
-            if((event.target.scrollTop === event.target.scrollTopMax) && Animals.style.display === "none" ){
-                animals(charactersEmoji)
-                setUpToolTip()
-                // const Emoji_flex_animals = document.querySelectorAll('.Emoji-flex')
-                // console.log(Emoji_flex_animals)
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Food.style.display === "none" ){
-                food(charactersEmoji)
-                setUpToolTip()
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Activities.style.display === "none" ){
-                activity(charactersEmoji)
-                setUpToolTip()
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Travel.style.display === "none" ){
-                travel(charactersEmoji) 
-                setUpToolTip()
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Objects.style.display === "none" ){
-                object(charactersEmoji) 
-                setUpToolTip()
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Symbols.style.display === "none" ){
-                symbol(charactersEmoji) 
-                setUpToolTip()
-            } else if((event.target.scrollTop === event.target.scrollTopMax) && Flags.style.display === "none" ){
-                flag(charactersEmoji) 
-                setUpToolTip()
-            } else  {
-                return
-            }
-            
+    charactersEmoji.filter(e => {
+        if (e.category === "People") {
+            tabs_array[1].insertAdjacentHTML("afterbegin", content(e))
+            content(e);
         }
-    )
+    })
 
+    const Animals = document.querySelector('#Animals')
+    const Food = document.querySelector('#Food')
+    const Activities = document.querySelector('#Activities')
+    const Travel = document.querySelector('#Travel')
+    const Objects = document.querySelector('#Objects')
+    const Symbols = document.querySelector('#Symbols')
+    const Flags = document.querySelector('#Flags')
+
+    Emoji_list.addEventListener("scroll", function (event) {
+        if ((event.target.scrollTop === event.target.scrollTopMax) && Animals.style.display === "none") {
+            animals(charactersEmoji)
+            setUpToolTip()
+            // const Emoji_flex_animals = document.querySelectorAll('.Emoji-flex')
+            // console.log(Emoji_flex_animals)
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Food.style.display === "none") {
+            food(charactersEmoji)
+            setUpToolTip()
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Activities.style.display === "none") {
+            activity(charactersEmoji)
+            setUpToolTip()
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Travel.style.display === "none") {
+            travel(charactersEmoji)
+            setUpToolTip()
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Objects.style.display === "none") {
+            object(charactersEmoji)
+            setUpToolTip()
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Symbols.style.display === "none") {
+            symbol(charactersEmoji)
+            setUpToolTip()
+        } else if ((event.target.scrollTop === event.target.scrollTopMax) && Flags.style.display === "none") {
+            flag(charactersEmoji)
+            setUpToolTip()
+        } else {
+            return
+        }
+
+    })
+
+    const img_Emoji = document.querySelectorAll('.Emoji-img')
+    const zzz = document.querySelector('.zzz')
+
+    window.addEventListener('keydown', e => {
+        if (e.key === ":") {
+            // const xd = `<div class="testb"></div>`
+            // zzz.innerHTML = xd
+            // e.preventDefault()
+            // e.stopPropagation()
+            let x = e.clientX
+            let y = e.clientY
+            Emoji_panel.style.left = x + "px";
+            Emoji_panel.style.top = y + "px";
+            Emoji_panel.style.display = "block"
+
+        }
+    })
 
 }
 
 
 const animals = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Animals & Nature') {
-                    Animals.style.display = "block"
-                    tabs_array[2].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Animals & Nature') {
+            Animals.style.display = "block"
+            tabs_array[2].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const food = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Food & Drink') {
-                    Food.style.display = "block"
-                    tabs_array[3].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Food & Drink') {
+            Food.style.display = "block"
+            tabs_array[3].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const activity = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Activities') {
-                    Activities.style.display = "block"
-                    tabs_array[4].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Activities') {
+            Activities.style.display = "block"
+            tabs_array[4].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const travel = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Travel & Places') {
-                    Travel.style.display = "block"
-                    tabs_array[5].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Travel & Places') {
+            Travel.style.display = "block"
+            tabs_array[5].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const object = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Objects') {
-                    Objects.style.display = "block"
-                    tabs_array[6].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Objects') {
+            Objects.style.display = "block"
+            tabs_array[6].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const symbol = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Symbols') {
-                    Symbols.style.display  = "block"
-                    tabs_array[7].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Symbols') {
+            Symbols.style.display = "block"
+            tabs_array[7].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 const flag = (charactersEmoji) => {
-        charactersEmoji.filter( e => {
-                if (e.category === 'Flags') {
-                    Flags.style.display = "block"
-                    tabs_array[8].insertAdjacentHTML("afterbegin" , content(e))
-                    return content(e);
-                    
-                    }  
-                }
-            )
+    charactersEmoji.filter(e => {
+        if (e.category === 'Flags') {
+            Flags.style.display = "block"
+            tabs_array[8].insertAdjacentHTML("afterbegin", content(e))
+            return content(e);
+
         }
+    })
+}
 
 // Search Method
 const Search_Emoji = (charactersEmoji) => {
-        // creat input search event
-        search_input.addEventListener('keyup' , (e) => {
-            const input_string = e.target.value;
-            const input_Uppercase = input_string.toLowerCase()
-            const filterEmoji = charactersEmoji.filter( e => {
-                return e.name.toLowerCase().includes(input_Uppercase)
-        }) 
-            displayEmoji(filterEmoji) 
+    // creat input search event
+    search_input.addEventListener('keyup', (e) => {
+        const input_string = e.target.value;
+        const input_Uppercase = input_string.toLowerCase()
+        const filterEmoji = charactersEmoji.filter(e => {
+            return e.name.toLowerCase().includes(input_Uppercase)
+        })
+        displayEmoji(filterEmoji)
     })
-        
+
 }
 
-search_input.addEventListener('input' , e => {
+search_input.addEventListener('input', e => {
     const act = document.querySelector('#act')
     console.log(e.target.value)
     if (e.target.value.length === 0) {
@@ -263,14 +271,14 @@ const displayEmoji = (filterEmoji) => {
                 <!--  END  -->`;
             })
             .join('')
-            const recent = document.querySelector('#recent')
-            recent.innerHTML = htmlString;
-        } else if (filterEmoji.length === 0)  {
-                const Nostring = '<span class="empty">No Result</span>'
-                recent.innerHTML = Nostring 
-                
-    } 
-} 
+        const recent = document.querySelector('#recent')
+        recent.innerHTML = htmlString;
+    } else if (filterEmoji.length === 0) {
+        const Nostring = '<span class="empty">No Result</span>'
+        recent.innerHTML = Nostring
+
+    }
+}
 
 
 // tooltip spawn
@@ -281,18 +289,18 @@ let setUpToolTip = () => {
     const tooltipElemArray = Array.from(toolTipElements)
     let timer;
 
-    let displayTooltip = (e , step) => {
+    let displayTooltip = (e, step) => {
         tooltip = step
         tooltipEmoji.innerHTML = tooltip
-        tooltipEmoji.style.top = e.pageY  + "px"
-        tooltipEmoji.style.left = e.pageX +  "px"
+        tooltipEmoji.style.top = e.pageY + "px"
+        tooltipEmoji.style.left = e.pageX + "px"
         fadeIN(tooltipEmoji)
-        }
+    }
 
-        let fadeOut = function(e) {
-            let op = 1;
-            if (!timer) {
-                timer = setInterval(() => {
+    let fadeOut = function (e) {
+        let op = 1;
+        if (!timer) {
+            timer = setInterval(() => {
                 if (op <= 0.1) {
                     clearInterval(timer);
                     timer = null
@@ -300,36 +308,36 @@ let setUpToolTip = () => {
                     e.style.display = "none";
                 }
                 e.style.opacity = op;
-                   op -= op * 0.1
+                op -= op * 0.1
             }, 10)
-                
+
+        }
+    }
+    let fadeIN = function (e) {
+        let op = 0.1;
+        e.style.display = "block"
+        var timer = setInterval(() => {
+            if (op >= 1) {
+                clearInterval(timer);
+
             }
-        }
-        let fadeIN = function(e) {
-            let op = 0.1;
-            e.style.display = "block"
-             var timer = setInterval(() => {
-                if (op >= 1) {
-                    clearInterval(timer);
-                    
-                }
-                e.style.opacity = op;
-                op += op * 0.1
-            }, 10)
-        }
+            e.style.opacity = op;
+            op += op * 0.1
+        }, 10)
+    }
 
     tooltipElemArray.forEach((e) => {
-            const step = e.dataset.tooltip
-            let timeout ;
-            e.addEventListener('mouseenter' , (e) => {
-    timeout = setTimeout(() => {
-                    displayTooltip(e , step)
-                },400)
-            });
+        const step = e.dataset.tooltip
+        let timeout;
+        e.addEventListener('mouseenter', (e) => {
+            timeout = setTimeout(() => {
+                displayTooltip(e, step)
+            }, 400)
+        });
 
-            e.addEventListener("mouseleave", e => {
-                clearTimeout(timeout)
-                fadeOut(tooltipEmoji)
+        e.addEventListener("mouseleave", e => {
+            clearTimeout(timeout)
+            fadeOut(tooltipEmoji)
         })
     })
 }
@@ -337,40 +345,29 @@ let setUpToolTip = () => {
 
 
 // Emoji Panel functions 
-buttons.forEach( e => e.addEventListener('click' , (tab) => 
-        {       
-                const openY = tab.target.dataset.btn
-                const open = document.querySelector(openY)
-                content_data.forEach( tabs => tabs.classList.remove('active'))
-                open.classList.toggle('active')
-        }
-))
+buttons.forEach(e => e.addEventListener('click', (tab) => {
+    const openY = tab.target.dataset.btn
+    const open = document.querySelector(openY)
+    content_data.forEach(tabs => tabs.classList.remove('active'))
+    open.classList.toggle('active')
+}))
 
 
 // Buttons trigger Emoji
-const click = trigger.addEventListener ('click' , function(e) {
-        e.preventDefault()
-        e.stopPropagation()
-        let x = e.clientX
-        let y = e.clientY
-        Emoji_panel.style.left = x + "px";
-        Emoji_panel.style.top = y + "px";
-        Emoji_panel.style.display = "block";
-}) 
+const click = trigger.addEventListener('click', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    let x = e.clientX
+    let y = e.clientY
+    Emoji_panel.style.left = x + "px";
+    Emoji_panel.style.top = y + "px";
+    Emoji_panel.style.display = "block";
+})
 
 
-document.addEventListener('mouseup', function(e) {
-  var container = document.querySelector('.Emoji-panel');
-  if (!container.contains(e.target)) {
-    container.style.display = 'none';
-  }
+document.addEventListener('mouseup', function (e) {
+    var container = document.querySelector('.Emoji-panel');
+    if (!container.contains(e.target)) {
+        container.style.display = 'none';
+    }
 });
-
-
-
-
-
-
-
-
-
